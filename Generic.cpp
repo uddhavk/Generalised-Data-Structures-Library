1f9b5b33d31ce84e8eb1f5494880f38ceed0f69c
Uddhav khatal <164346647+uddhavk@users.noreply.github.com>
2026-02-19 16:08:40 +0530

    Add files via upload

--- Generic.cpp
+++ Generic.cpp
@@ -7,8 +7,8 @@
 Type                        Name of class for node        Name of class for fucntinality
 -----------------------------------------------------------------------------------------
 Singly Linear               SinglyLLLnode                   SinglyLLL
-DOubly Linear               DoublyLLLnode                   DoublyLLL
-
+Doubly Linear               DoublyLLLnode                   DoublyLLL
+Singly Circular             SinglyCLLnode                   SinglyCLL
 -----------------------------------------------------------------------------------------
 */
 
@@ -690,6 +690,345 @@ int DoublyLLL<T>::Count()
     return this->iCount;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
+// Final code of Singly Circular LinlkedList using Generic Approach
+///////////////////////////////////////////////////////////////////////////////////////
+
+#pragma pack(1)
+template<class T>
+
+class SinglyCLLnode
+{
+    public: 
+        T data;
+        SinglyCLLnode<T> *next;
+
+        SinglyCLLnode(T no)
+        {
+            this->data = no;
+            this->next = NULL;
+        }
+};
+
+template <class T>
+class SinglyCLL
+{
+    private:
+        SinglyCLLnode<T> *first;
+        SinglyCLLnode<T> *last;
+        int iCount;
+
+    public:
+        SinglyCLL();
+
+        void InsertFirst(T);
+        void InsertLast(T);
+        void InsertAtPos(T,int);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int);
+
+        void Display();
+        int Count();    
+};
+
+template<class T>
+SinglyCLL<T> :: SinglyCLL()
+{
+    cout<<"Linked List gets created\n";
+    this->iCount = 0;
+    this->first = NULL;
+    this->last = NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertFirst
+//  Input         : Data of node
+//  Output        : Nothing
+//  Description   : Used to insert node at first position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<class T>
+void SinglyCLL<T> :: InsertFirst(T no)
+{
+    SinglyCLLnode<T> *newn = NULL;
+
+    newn = new SinglyCLLnode<T>(no);
+
+    if((this->first == NULL) && (this->last == NULL))
+    {
+        this->first = newn;
+        this->last = newn;
+    }
+    else
+    {
+        newn->next = this->first;
+        this->first = newn;
+    }
+    (this->last)->next = this->first;
+    this->iCount++;     
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertLast
+//  Input         : Data of node
+//  Output        : Nothing
+//  Description   : Used to insert node at last position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
+void SinglyCLL<T> :: InsertLast(T no)
+{
+    SinglyCLLnode<T> *newn = NULL;
+    newn = new SinglyCLLnode<T>(no);
+
+    if((this->first == NULL) && (this->last == NULL))
+    {
+        this->first = newn;
+        this->last = newn;
+    }
+    else 
+    {
+        this->last->next = newn;
+        this->last = newn;
+        
+    }
+    (this->last)->next = this->first;
+    this->iCount++; 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InseretAtPos
+//  Input         : Position of node
+//  Output        : node inserted
+//  Description   : Used to insert the node at given position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
+void SinglyCLL<T> :: InsertAtPos(T no,int pos)
+{
+    int iCnt = 0;
+    
+    SinglyCLLnode<T> *temp = NULL;
+    SinglyCLLnode<T> *newn = NULL;
+
+    if(pos < 1 || pos > (iCount + 1))
+    {
+        printf("Invalid Position");
+        return;
+    }
+    if(pos == 1)
+    {
+        this->InsertFirst(no);
+    }
+    else if(pos == (iCount + 1))
+    {
+        this->InsertLast(no);
+    }
+    else
+    {
+        temp = this->first;
+        newn = new SinglyCLLnode<T>(no);
+
+        newn->data = no;
+        newn->next = NULL;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp->next;
+        }
+
+        newn->next = temp->next;
+        temp->next = newn;
+        this->iCount++;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteFirst
+//  Input         : Nothing
+//  Output        : Delete First node
+//  Description   : Used to delete node at first position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<class T>
+void SinglyCLL<T> :: DeleteFirst()
+{
+    SinglyCLLnode<T> *temp = NULL;
+
+    if((this->first == NULL) && (this->last == NULL))
+    {
+        return;
+    }
+    else if(this->first == this->last)
+    { 
+        delete(this->first);              
+
+        this->first = NULL;
+        this->last = NULL;  
+    }
+    else
+    {
+        temp = this->first;
+        this->first = (this->first)->next;
+        delete(temp);
+
+        (this->last)->next = this->first;
+    }
+    this->iCount--;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteLast
+//  Input         : Nothing
+//  Output        : DeleteLast node
+//  Description   : Used to delete node at last position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
+void SinglyCLL<T> :: DeleteLast()
+{
+    SinglyCLLnode<T> *temp = NULL;
+
+    if((this->first == NULL) && (this->last == NULL))
+    {
+        return;
+    }
+    else if(this->first == this->last)
+    { 
+        delete(this->first);        
+        
+        this->first = NULL;
+        this->last = NULL; 
+    }
+    else
+    {
+        temp = this->first;
+        while(temp->next != this->last)
+        {
+            temp = temp->next;
+        }
+        delete(this->last);
+        this->last = temp;
+        temp->next = this->first;      
+    }
+    iCount--;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteAtPos
+//  Input         : Position of node
+//  Output        : node deleted
+//  Description   : Used to delete the node at given position
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<class T>
+void SinglyCLL<T> :: DeleteAtPos(int pos)
+{
+    int iCnt = 0;
+    
+    SinglyCLLnode<T> *temp = NULL;
+    SinglyCLLnode<T> *target = NULL;
+
+    if(pos < 1 || pos > (iCount + 1))
+    {
+        printf("Invalid Position");
+        return;
+    }
+    if(pos == 1)
+    {
+        this->DeleteFirst();
+    }
+    else if(pos == iCount)
+    {
+        this->DeleteLast();
+    }
+    else
+    {
+        temp = this->first;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp->next;
+        }
+        
+        target = temp->next;
+        temp->next = target->next;
+        delete(target);
+        this->iCount--;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Input         : Nothing
+//  Output        : Display Linked List
+//  Description   : Used to Display all nodes in a Linked List
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<class T>
+void SinglyCLL<T> :: Display()
+{
+    do
+    {
+        cout<<"| "<<first->data<<" |->";
+        first = first->next;
+    }while(first != last->next);
+    cout<<"\n";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Count
+//  Input         : Nothing
+//  Output        : Count
+//  Description   : Used to Count all nodes in a Linked List
+//  Author        : Uddhav Venkatesh Khatal
+//  Date          : 19/02/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
+int SinglyCLL<T> :: Count()
+{
+    return iCount;
+}
+
+
 //////////////////////////////////// End of Library //////////////////////////////////////////
 
 int main()
@@ -739,35 +1078,71 @@ int main()
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
     cout<<"\n--------------Doubly Linear Linked List-------------------\n";
     DoublyLLL<char> *dobj = new DoublyLLL<char>();
-
+    
     dobj->InsertFirst('c');
     dobj->InsertFirst('b');
     dobj->InsertFirst('a');
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
-
+    
     dobj->InsertLast('d');
     dobj->InsertLast('e');
     dobj->InsertLast('f');
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
-
+    
     dobj->DeleteFirst();
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
-
+    
     dobj->DeleteLast();
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
-
+    
     dobj->InsertAtPos('h',3);
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
-
+    
     dobj->DeleteAtPos(3);
     dobj->Display();
     cout<<"Total number of elements are : "<<dobj->Count()<<"\n";
     delete dobj;
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    cout<<"\n--------------Doubly Linear Linked List-------------------\n";
 
+    SinglyCLL<float> *scobj = new SinglyCLL<float>(); 
+
+    scobj->InsertFirst(51.3);
+    scobj->InsertFirst(21.2);
+    scobj->InsertFirst(11.1);
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    scobj->InsertLast(101.4);
+    scobj->InsertLast(121.5);
+    scobj->InsertLast(151.6);
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    scobj->DeleteFirst();
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    scobj->DeleteLast();
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    scobj->InsertAtPos(45.77,4);
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    scobj->DeleteAtPos(4);
+    scobj->Display();
+    cout<<"Total number of nodes are : "<<scobj->Count()<<"\n";
+
+    delete scobj;
+    
     return 0;
 }
